1eeb0b23a1a082e0e3aee30aae54ba69d5108788
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2022-06-15 17:13:55 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function resize(setWidth) {
+  setWidth(window.innerWidth);
+}
 
 function App() {
+  const [width, setWidth] = useState(window.innerWidth);
+  console.log("widht", width);
+  let content = <h1>Hello world</h1>;
+  if (width > 1200) {
+    content = (
+      <div>
+        Height: {window.innerHeight} Width: {width}
+      </div>
+    );
+  }
+  useEffect(() => {
+    window.addEventListener("resize", () => resize(setWidth));
+    return () => {
+      window.removeEventListener("resize", () => resize(setWidth));
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{content}</header>
     </div>
   );
 }
